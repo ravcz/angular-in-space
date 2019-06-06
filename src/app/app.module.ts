a50516e2f271283fc5e949f20548238032142ce6
Rafał <rafalczarnecki89@gmail.com>
2019-06-06 11:53:01 +0200

    task 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { A2sCommModule } from 'a2s-comm';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    A2sCommModule
   ],
   providers: [],
   bootstrap: [AppComponent]
