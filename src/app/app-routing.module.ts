cc1e8798b807305cbced62d69fe217e03f974f72
Rafał <rafalczarnecki89@gmail.com>
2019-06-08 13:55:09 +0200

    task 16

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { BlackHoleComponent } from './black-hole/black-hole.component';
 
 const routes: Routes = [
+  {path: 'intel', loadChildren: () => import('./intel/intel.module').then(m => m.IntelModule)},
   {path: '', redirectTo: 'space', pathMatch: 'full'},
   {path: '**', component: BlackHoleComponent}
 ];
---
+++ src/app/intel/intel-browser/intel-browser.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'app-intel-browser',
+  templateUrl: './intel-browser.component.html',
+  styleUrls: ['./intel-browser.component.scss']
+})
+export class IntelBrowserComponent implements OnInit {
+
+  options = [
+    {name: 'ESA', url: 'http://www.esa.int/ESA'},
+    {name: 'POLSA', url: 'https://www.polsa.gov.pl/pl'}
+  ];
+
+  source: SafeResourceUrl = null;
+
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
+
+  ngOnInit() {
+  }
+
+  selectSource(value: string) {
+    this.source = value ? this.sanitizer.bypassSecurityTrustResourceUrl(value) : null;
+  }
+
+}
