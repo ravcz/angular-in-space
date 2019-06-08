c285dafdb3c2822f082db44d3d8c56842a4b0457
Rafał <rafalczarnecki89@gmail.com>
2019-06-08 12:55:53 +0200

    task 15

--- src/app/space/pilot.service.ts
+++ src/app/space/pilot.service.ts
@@ -25,4 +25,24 @@ export class PilotService {
        map((pilotAttrs) => new Pilot(pilotAttrs))
     );
   }
+
+  savePilot(pilotAttrs: PilotAttrs): Observable<Pilot> {
+    if (pilotAttrs.id) {
+      return this.updatePilot(pilotAttrs);
+    } else {
+      return this.createPilot(pilotAttrs);
+    }
+  }
+
+  private createPilot(data: PilotAttrs): Observable<Pilot> {
+    return this.http.post<PilotAttrs>(`/api/pilots`, data).pipe(
+      map((pilotAttrs) => new Pilot(pilotAttrs))
+    );
+  }
+
+  private updatePilot(data: PilotAttrs): Observable<Pilot> {
+    return this.http.put<PilotAttrs>(`/api/pilots/${data.id}`, data).pipe(
+      map((pilotAttrs) => new Pilot(pilotAttrs))
+    );
+  }
 }
